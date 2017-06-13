5fcf983468827312403c7554d8ecf962670e52fc
AkshayPatil26 <akshpatil26@gmail.com>
2017-06-13 01:19:36 -0400

    Lec 98 Returning HTML Templates at localhost
    Added 2 html pages; Home page, About page
    Used render_template
    Added templates folder to save html pages

--- script.py
+++ script.py
@@ -1,14 +1,12 @@
-from flask import Flask
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "homepage here!"
 
 @app.route('/about')
 def about():
-    return "about website blah blah"
+
 
 if __name__=="__main__":
-    app.run (debug=True)
+    app.run (debug=True) #if this script is run; not if script is imported elsewhere
